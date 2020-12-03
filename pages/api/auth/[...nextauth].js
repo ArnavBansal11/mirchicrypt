6ca6b726b4d6bdfc69e1afa4d43996ce973a1edd
tperm94 <67191889+tperm94@users.noreply.github.com>
2020-12-03 08:42:14 +0530

    add env:

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -8,12 +8,11 @@ export default (req, res) =>
       Providers.Discord({
         clientId: "783612696930418699",
         clientSecret: "e8GuLWFgpAVwGy4C53Po0o1Sn7md5aV0",
-          scope: "identify email guilds",
-        
+        scope: "identify email guilds",
       }),
-      ],
-      
-      callbacks: {
+    ],
+
+    callbacks: {
       signIn: async (_user, _account, _profile) => {
         axios
           .post("http://mirchicrypt.herokuapp.com/auth/login", {
@@ -26,6 +25,5 @@ export default (req, res) =>
           })
           .catch((ex) => Promise.resolve(false));
       },
-      },
-      
+    },
   });
