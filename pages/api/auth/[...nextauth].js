76e9a4fe10c8628e883ed52dc49472152b638668
tperm94 <67191889+tperm94@users.noreply.github.com>
2020-12-03 10:24:21 +0530

    remove guilds access

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -8,14 +8,14 @@ export default (req, res) =>
       Providers.Discord({
         clientId: "783612696930418699",
         clientSecret: "e8GuLWFgpAVwGy4C53Po0o1Sn7md5aV0",
-        scope: "identify email guilds",
+        scope: "identify email guilds.join",
       }),
     ],
 
     callbacks: {
       signIn: async (_user, _account, _profile) => {
         axios
-          .post("http://mirchicrypt.herokuapp.com/auth/login", {
+          .post("http://localhost:5000/auth/login", {
             user: _user,
             account: _account,
             profile: _profile,
